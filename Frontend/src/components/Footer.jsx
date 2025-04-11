32a8d90dbde1c7fd7cd83c56424a69fcfc0cb1f0
yogitasharma15 <yogitaksp24@gmail.com>
2025-04-11 14:36:35 +0530

    first commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-links">
+          <a href="/Home">Home</a>
+          <a href="Volunteer">About</a>
+          <a href="Articles">Resources</a>
+          <a href="ContactUs">Contact</a>
+        </div>
+        <div className="footer-copyright">
+          &copy; {currentYear} Wellness.
+           All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
